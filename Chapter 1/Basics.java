eb32540115fd18c2134f9fce3b3872432e63c28b
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-08-05 21:55:30 +0530

    Initial Commit

---
+++ Chapter 1/Basics.java
@@ -0,0 +1,14 @@
+public class Basics {
+    public static void main(String[] args) {
+        System.out.println("My name is Piyush Shrivastava");
+        System.out.println("My age is 18;\nI lived in Bhopal");
+        System.out.println("I am pursuing B.Tech in AI/ML from Oriental College");
+
+
+
+
+
+
+
+    }
+}
