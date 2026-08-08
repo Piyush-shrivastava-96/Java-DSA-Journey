356293a1af10b994881e410e2d74610590d8e4f0
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-08-08 22:03:40 +0530

    Initial Commit📁

---
+++ Chapter 3/For_Loop.java
@@ -0,0 +1,40 @@
+
+public class For_Loop {
+    public static void main(String[] args) {
+
+        // for( int i = 1; i <=10; i++) {
+        // System.out.println(i);
+        // }
+
+        // for( int i = 10; i >= 1; i--) {
+        //     System.out.println(i);
+        // }
+
+
+        // int sum = 0;
+        // for ( int i = 0; i <= 110; i = i+1 ) {
+        //     sum = sum + i;
+
+        // }
+        // System.out.println("1 se 10 numbers ka addition; " +sum);
+
+
+        // for( int i = 1; i <= 20; i++) {
+        //     if(i%2 == 0) {
+        //         System.out.println(i);
+        //     }
+        // }
+
+
+        for( int i = 20; i >= 0; i--) {
+            if(i%2 == 0) {
+                System.out.println(i);
+            }
+        }
+
+
+        
+
+    }
+    
+}
