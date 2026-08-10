5a51ffe87bdd20d7befc285e9deed863e59710a9
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-08-10 20:15:58 +0530

    Initial Commit📁

--- Chapter 3/_03_Do_While_loop.java
+++ Chapter 3/_03_Do_While_loop.java
@@ -1,6 +1,32 @@
+import java.util.Scanner;
+
 public class _03_Do_While_loop {
     public static void main(String[] args) {
         
+        
+        // int i = 0;
+        // do {
+        //     System.out.println("count number; " +i);
+        //     i++;
+        // } while(i<=10); 
+        // System.out.println("Counst number over!");
+
+        // int i = 10;
+        // do{
+        //     System.out.println(i);
+        //     i--;
+        // } while(i>=1);
+        // System.out.println("Loop Over!");
+
+        int i = 1;
+        do{
+            if(i%3 == 0) {
+                System.out.println(i);
+            
+            } 
+            i++;
+        } while(i<=30);
+        System.out.println("Loop Is Over!");
     }
     
 }
