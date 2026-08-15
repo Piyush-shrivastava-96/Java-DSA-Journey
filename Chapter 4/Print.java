53adf7ce267d9b6597389898c4a8dbbd717e0b8c
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-08-15 09:20:46 +0530

    Initial Commit📁

--- Chapter 4/Print.java
+++ Chapter 4/Print.java
@@ -2,17 +2,43 @@
 public class Print {
     public static void main(String[] args) {
 
-        // int i = 4;
-        // for( int i = 1; i <=4; i++) {
-        //     for(int j = 1; j <= 5; j++){
-        //         System.out.print("*");
-        //     }
-        //     System.out.println();
-        // }
+    
+        for( int i = 1; i <=4; i++) {
+            for(int j = 1; j <= 5; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        
+        System.out.println("\n");
 
-        // int a = 4;
-        // int s = 5;
-        // for( int i = 1; i <= 4; i++) 
+        for( int i = 1; i <= 5; i++) {
+            for(int j = 1; j <= 5; j++) {
+                if(i==1 || i == 5 || j == 1 || j == 5 ){
+                    System.out.print("*");
+
+                }
+                else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+
+        System.out.println("\n");
+        
+            for(int k = 1; k <= 9; k++){
+                for( int j = 1; j <= 9; j++){
+                    if( k == 1 || k == 2 || k == 8 || k == 9 || j == 1 || j == 2 || j == 8 || j == 9  ) {
+                        System.out.print("*");
+                    }
+                    else{
+                        System.out.print(" ");
+                    }
+
+                }
+                System.out.println();
+                }
             
     }
         
