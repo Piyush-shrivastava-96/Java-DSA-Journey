600698708ff7e9ac5e8c2d4fb94c5cf2e8a68386
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-08-20 22:07:45 +0530

    Initial Commit📁

--- Chapter 4/Print.java
+++ Chapter 4/Print.java
@@ -51,8 +51,65 @@ public class Print {
             }
             System.out.println();
         }
-            
-    }
+
+        for(int h = 1; h <= 9; h++) {
+            for(int g = 1; g <= h; g++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
         
+    System.out.println("\n**************************************\n");
+
+    for(int b = 9; b >= 1; b--){
+        for(int v = b; v >= 1; v--){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    for(int x = 1; x <= 4; x++){
+        for(int z = 1; z <= x-1; z++){
+            System.out.print(" ");
+        }
+        for(int z = 1; z <= 5-x; z++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+
+    for(int i = 1; i <= 6; i++){
+        for(int j = 1; j <= 6-i+1; j++){
+            System.out.print(j +"");
+        }
+        System.out.println();
+    }
+
+
+    int number = 1;
+    for(int i = 1; i<=5; i++){
+        for(int j=1; j<=i; j++){
+            int sum = i + j;
+            if(sum % 2 == 0){
+                System.out.print("1");
+
+            }
+            else{
+                System.out.print("0");
+            }
+            
+        }
+        System.out.println();
+    }
+    int counter = 1;
+    for(int i=1; i<=5; i++){
+        for(int j = 1; j<=i; j++){
+            System.out.print(counter + " ");
+            counter++;
+
+        }
+        System.out.println();
+    }
     
+ }
 }
