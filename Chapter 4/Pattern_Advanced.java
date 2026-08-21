ced65594f046715e496a77ac50c61b8ea6b71a2d
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-08-21 20:21:58 +0530

    Initial Commit📁

--- Chapter 4/Pattern_Advanced.java
+++ Chapter 4/Pattern_Advanced.java
@@ -1,25 +1,35 @@
 public class Pattern_Advanced {
     public static void main(String[] args){
-        for(int i = 1; i <= 4; i++){
-            for(int j=1; j <= i; j++){
-                System.out.print("*");
-            }
-            for(int j = 1; j<=2*(4-i); j++){
+        // for(int i = 1; i <= 4; i++){
+        //     for(int j=1; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int j = 1; j<=2*(4-i); j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j = 1; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i = 4; i >= 1; i--){
+        //     for(int j=1; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int j = 1; j<=2*(4-i); j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j = 1; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        for(int i = 1; i <= 5; i++){
+            for(int j = 1; j<=5-i; j++){
                 System.out.print(" ");
             }
-            for(int j = 1; j <= i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for(int i = 4; i >= 1; i--){
-            for(int j=1; j <= i; j++){
-                System.out.print("*");
-            }
-            for(int j = 1; j<=2*(4-i); j++){
-                System.out.print(" ");
-            }
-            for(int j = 1; j <= i; j++){
+            for(int j = 1; j <= 5; j++){
                 System.out.print("*");
             }
             System.out.println();
