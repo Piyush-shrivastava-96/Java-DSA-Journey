c49093794ccd9679eb07aa3aa3c80513aefd1151
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-08-22 10:45:52 +0530

    Initial Commit📁

--- Chapter 4/Pattern_Advanced.java
+++ Chapter 4/Pattern_Advanced.java
@@ -1,60 +1,76 @@
 public class Pattern_Advanced {
     public static void main(String[] args){
-        for(int i = 1; i <= 4; i++){
-            for(int j=1; j <= i; j++){
-                System.out.print("*");
-            }
-            for(int j = 1; j<=2*(4-i); j++){
-                System.out.print(" ");
-            }
-            for(int j = 1; j <= i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for(int i = 4; i >= 1; i--){
-            for(int j=1; j <= i; j++){
-                System.out.print("*");
-            }
-            for(int j = 1; j<=2*(4-i); j++){
-                System.out.print(" ");
-            }
-            for(int j = 1; j <= i; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        // for(int i = 1; i <= 4; i++){
+        //     for(int j=1; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int j = 1; j<=2*(4-i); j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j = 1; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i = 4; i >= 1; i--){
+        //     for(int j=1; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     for(int j = 1; j<=2*(4-i); j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j = 1; j <= i; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
 
-        for(int i = 1; i <= 5; i++){
-            for(int j = 1; j<=5-i; j++){
-                System.out.print(" ");
-            }
-            for(int j = 1; j <= 5; j++){
+        // for(int i = 1; i <= 5; i++){
+        //     for(int j = 1; j<=5-i; j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int j = 1; j <= 5; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        // for(int i = 1; i<=5; i++){
+        //     for(int j=1; j<=5-i; j++){
+        //         System.out.print(" ");
+
+        //     }
+        //     for(int j = 1; j<=2*i-1; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+        // for(int i = 5; i>=1; i--){
+        //     for(int j=1; j<=5-i; j++){
+        //         System.out.print(" ");
+
+        //     }
+        //     for(int j = 1; j<=2*i-1; j++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        for(int i=8; i>=1; i--){
+            for(int j=3*i-2; j>=1; j--){
                 System.out.print("*");
+
             }
             System.out.println();
         }
-
-        for(int i = 1; i<=5; i++){
-            for(int j=1; j<=5-i; j++){
-                System.out.print(" ");
-
-            }
-            for(int j = 1; j<=2*i-1; j++){
+        for(int i=1; i<=8; i++){
+            for(int j=1; j <= 3*i-2; j++){
                 System.out.print("*");
+
             }
             System.out.println();
         }
-        for(int i = 5; i>=1; i--){
-            for(int j=1; j<=5-i; j++){
-                System.out.print(" ");
-
-            }
-            for(int j = 1; j<=2*i-1; j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
+        
         
     }
     
