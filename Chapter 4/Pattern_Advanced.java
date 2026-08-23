03e29e2eac00ef9ee006476f7e2c307220e606cc
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-08-23 16:32:10 +0530

    Initial Commit📁

--- Chapter 4/Pattern_Advanced.java
+++ Chapter 4/Pattern_Advanced.java
@@ -83,34 +83,47 @@ public class Pattern_Advanced {
         // }
 
 
-       for(int i=1; i<=4; i++){
-        for(int j=1; j<=4-i; j++){
-            System.out.print(" ");
-        }
-        for(int j=1; j<=i; j++){
-            if(j==1|| j==i ){
-                System.out.print("* ");
-            }
-            else{
-                System.out.print("  ");
-            }
-        }
-        System.out.println();
-       }
-       for(int i=4; i>=1; i--){
-        for(int j=1; j<=4-i; j++){
-            System.out.print(" ");
-        }
-        for(int j=1; j<=i; j++){
-            if(j==1 || j==i){
-            System.out.print("*"+" ");
-            }
-            else{
-                System.out.print("  ");
-            }
-        }
-        System.out.println();
-       }
+    //    for(int i=1; i<=4; i++){
+    //     for(int j=1; j<=4-i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1; j<=i; j++){
+    //         if(j==1|| j==i ){
+    //             System.out.print("* ");
+    //         }
+    //         else{
+    //             System.out.print("  ");
+    //         }
+    //     }
+    //     System.out.println();
+    //    }
+    //    for(int i=4; i>=1; i--){
+    //     for(int j=1; j<=4-i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1; j<=i; j++){
+    //         if(j==1 || j==i){
+    //         System.out.print("*"+" ");
+    //         }
+    //         else{
+    //             System.out.print("  ");
+    //         }
+    //     }
+    //     System.out.println();
+    //    }
+
+   for(int i=1; i<=5; i++){
+    for(int j=1; j<=5-i; j++){
+        System.out.print("  ");
+    }
+    for(int j=i; j>=1; j--){
+        System.out.print(j+" ");
+    }
+    for(int j=2; j<=i; j++){
+        System.out.print(j+" ");
+    }
+    System.out.println();
+   }
         
     }
     
