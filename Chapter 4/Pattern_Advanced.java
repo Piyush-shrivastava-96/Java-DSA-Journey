c0d59755552a909206e0b56c7b7887de5a630c6e
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-08-24 21:37:15 +0530

    Initial Commit📁

--- Chapter 4/Pattern_Advanced.java
+++ Chapter 4/Pattern_Advanced.java
@@ -126,18 +126,79 @@ public class Pattern_Advanced {
 //    }
 
 
-    for(int i=1; i<=6; i++){
-        for(int j=1; j<=6-i; j++){
-            if(i==1 || j==1 || j==6-i){   
-                System.out.print(j+" "); 
+    // for(int i=1; i<=6; i++){
+    //     for(int j=1; j<=6-i; j++){
+    //         if(i==1 || j==1 || j==6-i){   
+    //             System.out.print(j+" "); 
+    //         }
+    //         else{
+    //             System.out.print(" ");
+    //         }
+
+    //     }
+    //     System.out.println();
+    // }
+    
+    
+    // for(int i=1; i<=5; i++){
+    //     for(int j=1; j<=5-i; j++){
+    //         System.out.print(" ");
+    //     }
+    //     for(int j=1; j<=5; j++){
+    //     System.out.print("*");
+    //     }
+    //     System.out.println();
+    // }
+
+    // Home Work ;-
+ 
+    for(int i=1; i<=4; i++){
+        for(int j=1; j<=i; j++){
+            if(j==1 || j==i){
+            System.out.print("*");
             }
             else{
                 System.out.print(" ");
             }
-
+        }
+        for(int j=1; j<=2*(4-i); j++){
+            System.out.print(" ");
+        
+        }
+        for(int j=1; j<=i; j++){
+            if(j==1 || j==i){
+            System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
         }
         System.out.println();
-    }
+        }
+        for(int i=4; i>=1; i--){
+        for(int j=1; j<=i; j++){
+            if(j==1 || j==i){
+            System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+        }
+        for(int j=1; j<=2*(4-i); j++){
+            System.out.print(" ");
+        
+        }
+        for(int j=1; j<=i; j++){
+             if(j==1 || j==i){
+            System.out.print("*");
+            }
+            else{
+                System.out.print(" ");
+            }
+            
+        }
+        System.out.println();
+        }
         
     }
     
