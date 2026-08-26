3a2a8c0ca3c85e4f6708b277e5561b66dfd46698
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-08-26 14:31:52 +0530

    Initial Commit📁

--- Chapter 4/Pattern_Advanced.java
+++ Chapter 4/Pattern_Advanced.java
@@ -214,6 +214,26 @@ public class Pattern_Advanced {
         }
         System.out.println();
     }
+
+    for(int i=1; i<=5; i++){
+        for(int j=1; j<=5-i; j++){
+            System.out.print(" ");
+        }
+        for(int j=1; j<=i*2; j++){
+            System.out.print(j);
+        }
+        System.out.println();
+    }
+
+    for(int i=1; i<=5; i++){
+        for(int j=1; j<=5-i; j++){
+            System.out.print(" ");
+        }
+        for(int j=1; j<=i; j++){
+            System.out.print(j+" ");
+        }
+        System.out.println();
+    }
         
     }
     
