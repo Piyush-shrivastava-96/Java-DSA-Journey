7caa08d5b0995405e3dcf6cd5e3f58e3811b9ba4
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-04 14:12:22 +0530

    Initial Commit📁

--- Chapter 2/_02_Switch_case.java
+++ Chapter 2/_02_Switch_case.java
@@ -74,6 +74,44 @@ public class _02_Switch_case {
         
         }
 
+         System.out.println("\n                          4. Use of Switch Case;--~~");
+
+         Scanner kc = new Scanner(System.in);
+        
+        System.out.println("Please enter the number only from 1 to 12 :-");
+        int op = kc.nextInt();
+
+        switch (op) {
+            case 1 : System.out.println("Number "+op+ " Represents By - 'January'");
+            break;
+            case 2 : System.out.println("Number "+op+ " Represents By - 'February'");
+            break;
+            case 3 : System.out.println("Number "+op+ " Represents By - 'March'");
+            break;
+            case 4 : System.out.println("Number "+op+ " Represents By - 'April'");
+            break;
+            case 5 : System.out.println("Number "+op+ " Represents By - 'May'");
+            break;
+            case 6 : System.out.println("Number "+op+ " Represents By - 'June'");
+            break;
+            case 7 : System.out.println("Number "+op+ " Represents By - 'July'");
+            break;
+            case 8 : System.out.println("Number "+op+ " Represents By - 'August'");
+            break;
+            case 9 : System.out.println("Number "+op+ " Represents By - 'September'");
+            break;
+            case 10 : System.out.println("Number "+op+ " Represents By - 'October'");
+            break;
+            case 11 : System.out.println("Number "+op+ " Represents By - 'November'");
+            break;
+            case 12 : System.out.println("Number "+op+ " Represents By- 'December'");
+            break;
+            default : System.out.println("Invalid Input, Error!");
+        }
+        
+        
+        
+
 
         
 
--- Chapter 5/Practice.java
+++ Chapter 5/Practice.java
@@ -2,41 +2,7 @@ import java.util.*;
 
 public class Practice {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        
-        System.out.println("Please enter the number\nWhat do you want ? This number will give you information about the month\nPlease enter the number only from 1 to 12");
-        int op = sc.nextInt();
-
-        switch (op) {
-            case 1 : System.out.println("Number "+op+ " Represents By - 'January'");
-            break;
-            case 2 : System.out.println("Number "+op+ " Represents By - 'February'");
-            break;
-            case 3 : System.out.println("Number "+op+ " Represents By - 'March'");
-            break;
-            case 4 : System.out.println("Number "+op+ " Represents By - 'April'");
-            break;
-            case 5 : System.out.println("Number "+op+ " Represents By - 'May'");
-            break;
-            case 6 : System.out.println("Number "+op+ " Represents By - 'June'");
-            break;
-            case 7 : System.out.println("Number "+op+ " Represents By - 'July'");
-            break;
-            case 8 : System.out.println("Number "+op+ " Represents By - 'August'");
-            break;
-            case 9 : System.out.println("Number "+op+ " Represents By - 'September'");
-            break;
-            case 10 : System.out.println("Number "+op+ " Represents By - 'October'");
-            break;
-            case 11 : System.out.println("Number "+op+ " Represents By - 'November'");
-            break;
-            case 12 : System.out.println("Number "+op+ " Represents By- 'December'");
-            break;
-            default : System.out.println("Invalid Input, Error!");
-        }
-        
-        
-        
+       
         
     }
     
