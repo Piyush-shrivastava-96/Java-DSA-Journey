76c108043b60bf07f4be3ee29e004aeaf7869374
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-05 23:28:59 +0530

    Initial Commit📁

--- Chapter 5/Practice.java
+++ Chapter 5/Practice.java
@@ -4,7 +4,7 @@ public class Practice {
     public static void main(String[] args) {
         // Scanner sc = new Scanner(System.in);
 
-    
+    System.out.println("Hello, duniya");
     }
     
 }
