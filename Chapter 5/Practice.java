82ecda4e3e1012941670d2e69d2dbc8c0e47556b
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-05 23:27:15 +0530

    Initial Commit📁

--- Chapter 5/Practice.java
+++ Chapter 5/Practice.java
@@ -4,18 +4,7 @@ public class Practice {
     public static void main(String[] args) {
         // Scanner sc = new Scanner(System.in);
 
-      for(int i=1; i<=5; i++){
-        for(int j=1; j<=5-i; j++){
-            System.out.print(" ");
-        }
-        for(int j=1; j<=5; j++){
-            if(i==1 || i==5 || j==1 || j==5)
-            System.out.print("*");
-            else
-            System.out.print(" ");
-        }
-        System.out.println();
-      }
+    
     }
     
 }
