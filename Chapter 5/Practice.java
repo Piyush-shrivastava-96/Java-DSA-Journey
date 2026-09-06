9f234b998e259c8d61e3d6a034381ed1d4ca6d18
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-06 21:26:20 +0530

    Initial Commit📁

--- Chapter 5/Practice.java
+++ Chapter 5/Practice.java
@@ -3,20 +3,21 @@ import java.util.*;
 public class Practice {
     
         public static void findPrime(int n){
-        // for(int i=2; i<n; i++){
-        //    if(n%i==0){
-        //     System.out.println("it is a composite  number");
-        //     return;
-        //    }
-        // }
-        //  System.out.println("it is a prime number");
-        // }
+        for(int i=2; i<n; i++){
+           if(n%i==0){
+            System.out.println("it is a composite  number");
+            return;
+           }
+        }
+         System.out.println("it is a prime number");
+        }
 
-        // public static void main(String[] args) {
-        // Scanner sc = new Scanner(System.in);
-        // int n = sc.nextInt();
+        public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
 
-        // findPrime(n);
+        findPrime(n);
+        System.out.println();
         }
     
 }
