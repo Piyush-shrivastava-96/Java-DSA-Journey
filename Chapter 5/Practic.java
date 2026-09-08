a5bfc4ae8d56ff2d16d6b1fa829c2cdcd9645228
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-08 23:56:46 +0530

    Initial Commit📁

---
+++ Chapter 5/Practic.java
@@ -0,0 +1,6 @@
+public class Practic {
+    public static void main(String[] args){
+        System.out.println("Hello Salman bhai");
+    }
+    
+}
--- Chapter 5/Practice.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-
-public class Practice {
-    
-        public static void findPrime(int n){
-    
-        }
-    
-}
