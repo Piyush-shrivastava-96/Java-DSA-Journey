0f2843053dfcbe02f017e15d524279178ca9a8a6
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-09 01:28:32 +0530

    Initial Commit📁

--- Chapter 5/Practic.java
+++ Chapter 5/Practic.java
@@ -1,6 +1,36 @@
-public class Practic {
+import java.util.*;
+
+public class Practic {  
+    public static void  findPrime(int n){
+        if(n<=1){
+            System.out.println("Invalid Number");
+           return;
+        }
+        
+        int t = 1;
+        for(int i=1; i<n; i++){
+            if(n%i==0){
+            t++;
+        }
+        }
+         if(t==2){
+            System.out.println(n+" is a prime number");
+        }
+        else{
+            System.out.println(n+" is not a prime number");
+        }
+        
+
+    }
+      
+
     public static void main(String[] args){
-        System.out.println("");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number");
+        int n = sc.nextInt();
+        System.out.println("So the answer is :-\n");
+        findPrime(n);
+       
     }
     
 }
