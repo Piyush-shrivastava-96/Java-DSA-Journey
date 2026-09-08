584bceabb4d7c23b33b30bdce9675223659930bc
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-08 23:59:31 +0530

    Initial Commit📁

--- Chapter 5/Practic.java
+++ Chapter 5/Practic.java
@@ -1,6 +1,6 @@
 public class Practic {
     public static void main(String[] args){
-        System.out.println("Hello Salman bhai\nI am your big fan.");
+        System.out.println("");
     }
     
 }
