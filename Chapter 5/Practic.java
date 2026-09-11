2495d2827ca68a5bf2c4f6ca7188eb6b06187b6c
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-11 23:50:18 +0530

    Initial Commit📁

--- Chapter 5/Practic.java
+++ Chapter 5/Practic.java
@@ -9,14 +9,15 @@ public class Practic {
     // }
       
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter the number");
-        int n = sc.nextInt();
-        System.out.println("So the answer is :-\n");
-        // findAverage();
+    // public static void main(String[] args){
+    //     Scanner sc = new Scanner(System.in);
+    //     System.out.println("Enter the number");
+    //     int n = sc.nextInt();
+    //     System.out.println("So the final answer is :-\n");
+    //     // findAverage();
+     
         
        
-    }
+    // }
     
 }
