b1b27ceb6ad19004a72237248d3c6fd3465969ca
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-13 19:45:20 +0530

    Initial Commit📁

--- Chapter 5/Practic.java
+++ Chapter 5/Practic.java
@@ -1,35 +1,24 @@
 import java.util.*;
 
 public class Practic {  
-    public static int findGreater(int n, int m){
-        int ans = 1;
-        if(n>m){
-           ans = n;
-        }
-        else if(n<m){
-            ans = m;
-        }
-       
-        else{
-            ans = n;
-        }
-        return ans;
+    public static int findSum(int n){
+        int sum = 0;
+        for(int i=1; i<=n; i++){
+        
+        sum = sum+i;
+      }
+      
+      return sum;
          
     }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
         int n = sc.nextInt();
-        System.out.println("Enter the second number");
-        int m = sc.nextInt();
-        int answer = findGreater(n, m);
-        if(n==m){
-            System.out.println("Both Numbers are Equal");
-        }
-        else{
-        System.out.println("So the greater is -: " +answer);
-        }
-
+        System.out.println("So the answer is -:");
+        int ans = findSum(n);
+        System.out.println(+ans);
+       
 
     
     }
