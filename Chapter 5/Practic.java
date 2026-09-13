18528cbb04c166a00646a14d27bfd0ff42f2e494
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-13 19:47:10 +0530

    Initial Commit📁

--- Chapter 5/Practic.java
+++ Chapter 5/Practic.java
@@ -1,24 +1,16 @@
 import java.util.*;
 
 public class Practic {  
-    public static int findSum(int n){
-        int sum = 0;
-        for(int i=1; i<=n; i++){
-        
-        sum = sum+i;
-      }
-      
-      return sum;
+    // public static int findSum(int n){
+     
          
-    }
+    // }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter the number");
         int n = sc.nextInt();
         System.out.println("So the answer is -:");
-        int ans = findSum(n);
-        System.out.println(+ans);
-       
+      
 
     
     }
