4c0562d51d8316ff72ee33b733a29cfd520a318c
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-15 21:46:25 +0530

    Initial Commit📁

---
+++ Chapter 6/Ary_cls.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Ary_cls {
+    public static void main(String args []){
+        // Scanner sc = new Scanner(System.in);
+        // int size = sc.nextInt();
+        // int[] numbers = new int [size];
+        // for(int i=0; i<size; i++){
+        //     numbers[i] = sc.nextInt();
+        // }
+        // System.out.println("Enter the number you want to find ");
+        // int x = sc.nextInt();
+
+
+        // for(int i=0; i<numbers.length; i++){
+        //     if(numbers[i] == x){
+        //     System.out.println("x found at index " + i);
+        //     }
+        // }
+        
+ }
+}
+
