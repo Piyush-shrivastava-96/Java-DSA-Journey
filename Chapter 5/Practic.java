03cf7ecd65f779e5d44dcc25739bd6e39327bd65
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-19 00:49:33 +0530

    Initial Commit📁

--- Chapter 5/Practic.java
+++ Chapter 5/Practic.java
@@ -5,11 +5,35 @@ public class Practic {
        
         
     // }
+    
     public static void main(String[] args){
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter your age");
-        // int a = sc.nextInt();
-        
+   
+    Scanner sc = new Scanner(System.in);
+    int positive = 0;
+    int minus = 0;
+    int zero = 0; 
+    while (true){
+    System.out.println("Enter the number");
+    int number = sc.nextInt();
+    if(number > 0){
+        positive++;
+    }
+    else if(number < 0){
+        minus--;
+    }
+    else if(number == 0){
+        zero++;
+    }
+
+    System.out.println("Positive numbers are total = " +positive);
+    System.out.println("Minus numbers are total = " +minus);
+    System.out.println("Zero numbers are total = " +zero);
+
+    System.out.println("you want to continue this game press '1' and '0' ");
+    // if(press == 1){
+    //     System.out.println("Okay we are continue this game ");
+    // }
+}
         
         
         
