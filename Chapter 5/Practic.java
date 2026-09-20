402a59d7d21b5e709c63d034847289da222511ad
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-20 09:18:29 +0530

    Initial Commit📁

--- Chapter 5/Practic.java
+++ Chapter 5/Practic.java
@@ -1,48 +1,30 @@
 import java.util.*;
 
 public class Practic {  
-    // public static void find(int n){
-       
+    // public static void find(int x, int n){
+      
         
     // }
     
     public static void main(String[] args){
    
     Scanner sc = new Scanner(System.in);
-    int positive = 0;
-    int minus = 0;
-    int zero = 0; 
-    while (true){
-    System.out.println("Enter the number");
-    int number = sc.nextInt();
-    if(number > 0){
-        positive++;
-    }
-    else if(number < 0){
-        minus--;
-    }
-    else if(number == 0){
-        zero++;
-    }
+    
+    
+    
+    
+   System.out.println("Enter the number of x");
+   int x = sc.nextInt();
+   System.out.println("Enter the power of n");
+   int n = sc.nextInt();
+   int result = 1;
+   for(int i=1; i<=n; i++){
+    result = result*x;
+   }
+   System.out.println(+x+" ki power " +n+ " ka result = " +result);
 
-    System.out.println("Positive numbers are total = " +positive);
-    System.out.println("Minus numbers are total = " +minus);
-    System.out.println("Zero numbers are total = " +zero);
+    
 
-    System.out.println("you want to continue this game press '1' and '0' ");
-    int press = sc.nextInt();
-    if(press == 1){
-        System.out.println("Okay we are continue this game ");
-    }
-    else if(press == 0){
-        System.out.println("Okay We are stop");
-        break;
-    }
-    else{
-        System.out.println("Invalid input!");
-        break;
-    }
-}
         
         
         
