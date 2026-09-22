d79dda8c4884f096453faa82328aa7470b227648
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-22 21:18:43 +0530

    Initial Commit📁

--- Chapter 5/Practic.java
+++ Chapter 5/Practic.java
@@ -1,46 +1,26 @@
 import java.util.*;
 
 public class Practic {  
-    // public static void findGcd(int a, int b){
-    //     int gcd = 1; 
-    //    for(int i=1; i<=a && i<=b; i++){
-    //     if(a%i == 0 && b%i == 0){
-    //         gcd = i;
-
-    //     }
-    //    }
-    //    System.out.println("GCD Number is = " +gcd);
-        
-    // }
+    // public static void findGcd(int n){
+ 
     
-    public static void main(String[] args){
+
+    // }
+
+        public static void main(String[] args){
    
     Scanner sc = new Scanner(System.in);
-    int n1 = sc.nextInt();
-    int n2 = sc.nextInt();
-    while(n1 != n2){
-    if(n1>n2){
-        n1 = n1 - n2;
+    int n = sc.nextInt();
+    int a = 0; 
+    int b = 1;
+    for(int i=1; i<=n; i++){
+        System.out.print(b+" ");
+        int next = b;
+        b = a+b;
+        a = next;
     }
-    else{
-        n2 = n2 - n1;
-    }
-    }
-System.out.println(n1);
-//    System.out.println("Enter the number of a");
-//    int a = sc.nextInt();
-//    System.out.println("Enter the power of b");
-//    int b = sc.nextInt();
-
-//    findGcd(a, b);
-  
-
-        
-        
-        
-      
-
-    
+    System.out.println();
+     System.out.println();
     }
 
     
