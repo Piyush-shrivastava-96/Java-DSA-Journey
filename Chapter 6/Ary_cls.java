91c783cbc9db51a898d0b682eab53240ce4d39b4
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-23 21:29:55 +0530

    Initial Commit📁

--- Chapter 6/Ary_cls.java
+++ Chapter 6/Ary_cls.java
@@ -3,7 +3,14 @@ import java.util.*;
 public class Ary_cls {
     public static void main(String args []){
         Scanner sc = new Scanner(System.in);
-        
+        int size = sc.nextInt();
+        String names[] = new String[size];
+        for(int i=1; i<size; i++){
+             names[i] = sc.next();
+        }
+        for(int i=1; i<names.length; i++){
+            System.out.println(names[i]);
+        }
        
         
  }
