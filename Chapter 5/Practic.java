ee31cc93d5907e43712cc4bac52a5db82ba485f5
Piyush Shrivastava <piyush.shrivastava.9669@gmail.com>
2026-09-23 21:18:50 +0530

    Initial Commit📁

--- Chapter 5/Practic.java
+++ Chapter 5/Practic.java
@@ -1,27 +1,11 @@
 import java.util.*;
 
 public class Practic {  
-    // public static void findGcd(int n){
- 
-    
-
-    // }
 
         public static void main(String[] args){
    
     Scanner sc = new Scanner(System.in);
-    int n = sc.nextInt();
-    int a = 0; 
-    int b = 1;
-    for(int i=1; i<=n; i++){
-        System.out.print(b+" ");
-        int next = b;
-        b = a+b;
-        a = next;
-    }
-    System.out.println();
     
-    }
-
     
+}
 }
--- Chapter 6/Ary_cls.java
+++ Chapter 6/Ary_cls.java
@@ -4,19 +4,14 @@ public class Ary_cls {
     public static void main(String args []){
         Scanner sc = new Scanner(System.in);
         int size = sc.nextInt();
-        int[] numbers = new int [size];
-        for(int i=0; i<size; i++){
-            numbers[i] = sc.nextInt();
+        String name[] = new String[size];
+        for(int i=0; i < size; i++){
+            name[i] = sc.next();
         }
-        System.out.println("Enter the number you want to find ");
-        int x = sc.nextInt();
-
-
-        for(int i=0; i<numbers.length; i++){
-            if(numbers[i] == x){
-            System.out.println("x found at index = " + i);
-            }
+        for(int i=0; i<name.length; i++){
+            System.out.println(name[i]);
         }
+       
         
  }
 }
